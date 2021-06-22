1d06a12b99bd3d14eafcf2180e3f8e74130212be
Santino L Ramos <santinoramos@fb.com>
2021-06-22 18:39:58 -0400

    First commit with required features implemented

---
+++ Tipster/TipViewController.m
@@ -0,0 +1,54 @@
+//
+//  TipViewController.m
+//  Tipster
+//
+//  Created by Santino L Ramos on 6/22/21.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipPercentageControl;
+
+@end
+
+@implementation TipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+ 
+- (IBAction)onTap:(id)sender {
+    NSLog(@"print");
+    [self.view endEditing:true];
+}
+
+- (IBAction)UpdateLabels:(id)sender {
+    double tipPercentages[]  = {0.15, 0.2, 0.25};
+    double tipPercentage = tipPercentages[self.tipPercentageControl.selectedSegmentIndex];
+    
+    double bill = [self.billField.text doubleValue];
+    double tip = bill * tipPercentage;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
